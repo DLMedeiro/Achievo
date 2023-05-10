67f65c158f1cc60b1843c042f6248ae239ad45fc
DanielleM <dlmedeiro@gmail.com>
2023-05-10 10:06:33 -0700

    clean routes file

--- backend/routes/goalRoutes.ts
+++ backend/routes/goalRoutes.ts
@@ -5,9 +5,14 @@ const router = express.Router()
 
 const {getGoals, setGoal, updateGoal, deleteGoal} = require('../controllers/goalController')
 
-router.get('/', getGoals)
-router.post('/', setGoal)
-router.put('/:id', updateGoal)
-router.delete('/:id', deleteGoal)
+router.route('/').get( getGoals).post(setGoal)
+// Above code simplifies the code below
+// router.get('/', getGoals)
+// router.post('/', setGoal)
+
+router.route('/:id').delete( deleteGoal).put( updateGoal)
+// Above code simplifies the code below
+// router.put('/:id', updateGoal)
+// router.delete('/:id', deleteGoal)
 
 module.exports = router
