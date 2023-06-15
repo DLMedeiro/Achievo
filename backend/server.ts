9addfb1c1549bca0e844b841ed2852c228b92c89
DanielleM <dlmedeiro@gmail.com>
2023-06-14 22:47:37 -0700

    update import

--- backend/server.ts
+++ backend/server.ts
@@ -20,8 +20,9 @@ const app = express();
 const cors = require("cors");
 
 // Bring in error handler
-import errorHandler from "./middleware/errorMiddleware";
-// changed to export default on middleware to eliminate errors when bringing the function to this file
+// import errorHandler from "./middleware/errorMiddleware";
+const errorHandler = require("./middleware/errorMiddleware");
+// changed to export default on middleware to eliminate errors when bringing the function to this file ?
 
 // add this middleware so req.body within the controller will not show as undefined
 // body parser for raw json
