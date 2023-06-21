ba3aa574bc6aa754b52be53418ac13e583a40098
DanielleM <dlmedeiro@gmail.com>
2023-06-21 12:36:35 -0700

    remove frotend auto start

--- backend/server.ts
+++ backend/server.ts
@@ -43,19 +43,19 @@ app.use('/api/goals', goalRoutes)
 app.use('/api/users', userRoutes)
 
 // Serve frontend
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend/build')))
-  //
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '../frontend/build')))
+//   //
 
-  app.get('*', (req, res) =>
-    res.sendFile(
-      path.resolve(__dirname, '../', 'frontend', 'build', 'index.html'),
-    ),
-  )
-  // * -> any route aside from API routes and point it towards the index.html file that is in the build folder
-} else {
-  app.get('/', (req, res) => res.send('Please set to production'))
-}
+//   app.get('*', (req, res) =>
+//     res.sendFile(
+//       path.resolve(__dirname, '../', 'frontend', 'build', 'index.html'),
+//     ),
+//   )
+//   // * -> any route aside from API routes and point it towards the index.html file that is in the build folder
+// } else {
+//   app.get('/', (req, res) => res.send('Please set to production'))
+// }
 
 app.use(errorHandler)
 // This will overwrite the express default handler
