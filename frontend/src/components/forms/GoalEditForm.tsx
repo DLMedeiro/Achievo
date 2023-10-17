3b54476678d206c183087aa657b419f985836723
DanielleM <dlmedeiro@gmail.com>
2023-10-17 16:07:54 -0700

    feat: change layout to match new goal

--- frontend/src/components/forms/GoalEditForm.tsx
+++ frontend/src/components/forms/GoalEditForm.tsx
@@ -1,84 +1,132 @@
-import React, { useState, useEffect } from 'react'
-import { useAppSelector, useAppDispatch } from '../../app/hooks'
-import { RootState } from '../../app/store'
-import { useNavigate } from 'react-router-dom'
-import { useForm, SubmitHandler } from 'react-hook-form'
-import Grid from '@mui/material/Grid'
-import Box from '@mui/material/Box'
-import TextField from '@mui/material/TextField'
-import Button from '@mui/material/Button'
-import dayjs, { Dayjs } from 'dayjs'
-import { zodResolver } from '@hookform/resolvers/zod'
-import { z } from 'zod'
-import { DatePicker } from '@mui/x-date-pickers/DatePicker'
-import { DemoItem } from '@mui/x-date-pickers/internals/demo'
-import { getGoals, changeGoal } from '../../features/goals/goalSlice'
-import { Link } from 'react-router-dom'
-import Typography from '@mui/material/Typography'
-import { Paper } from '@mui/material'
+import React, { useState, useEffect } from "react";
+import { useAppSelector, useAppDispatch } from "../../app/hooks";
+import { RootState } from "../../app/store";
+import { useNavigate } from "react-router-dom";
+import { useForm, SubmitHandler } from "react-hook-form";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import dayjs, { Dayjs } from "dayjs";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import { DemoItem } from "@mui/x-date-pickers/internals/demo";
+import { getGoals, changeGoal } from "../../features/goals/goalSlice";
+import { Link } from "react-router-dom";
+import Typography from "@mui/material/Typography";
+import { Paper } from "@mui/material";
+import { MobileDatePicker } from "@mui/x-date-pickers/MobileDatePicker";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 
 export default function ActivityInputForm() {
+  const textFieldStyles = {
+    MuiTextField: {
+      styleOverrides: {
+        root: {
+          // borderBottom: "2px solid transparent",
+          textAlign: "center",
+          fontSize: "1.2em",
+          "& .MuiInputBase-input": {
+            // paddingTop: "25px",
+            textAlign: "center",
+            fontSize: "1.2em",
+          },
+        },
+      },
+    },
+  };
+  const colorScheme = {
+    palette: {
+      primary: {
+        main: "#1e5b5e", // Change this to your desired primary color
+      },
+      secondary: {
+        main: "#61dafb", // Change this to your desired secondary color
+      },
+    },
+  };
+  const theme = createTheme({
+    ...colorScheme, // Include the color scheme
+    components: {
+      MuiTextField: {
+        styleOverrides: {
+          root: {
+            // borderBottom: "2px solid transparent",
+            textAlign: "center",
+            fontSize: "1em",
+            "& .MuiInputBase-input": {
+              // paddingTop: "25px",
+              textAlign: "center",
+              fontSize: "1.2em",
+            },
+          },
+        },
+      },
+    },
+  });
+
   interface Goal {
-    _id: string
-    user: string
-    activity: string
-    start: Date
-    end: Date
-    target: Number
-    progress: Number
-    createdAt: string
-    updatedAt: string
-    __v?: number
+    _id: string;
+    user: string;
+    activity: string;
+    start: Date;
+    end: Date;
+    target: Number;
+    progress: Number;
+    createdAt: string;
+    updatedAt: string;
+    __v?: number;
   }
   interface userState {
-    user: any
+    user: any;
   }
   interface goalState {
-    goals: Goal[]
-    isError: boolean
-    isSuccess: boolean
-    isLoading: boolean
-    message: string | undefined
+    goals: Goal[];
+    isError: boolean;
+    isSuccess: boolean;
+    isLoading: boolean;
+    message: string | undefined;
   }
   const { goals, isLoading, isError, message }: goalState = useAppSelector(
-    (state: RootState) => state.goals,
-  )
+    (state: RootState) => state.goals
+  );
 
-  const { user }: userState = useAppSelector((state: RootState) => state.auth)
+  const { user }: userState = useAppSelector((state: RootState) => state.auth);
 
-  const [changeBtn, setChangeBtn] = useState(true)
+  const [changeBtn, setChangeBtn] = useState(true);
 
-  const [startValue, setStartValue] = React.useState<Dayjs | null>(dayjs(0))
-  const [endValue, setEndValue] = React.useState<Dayjs | null>(dayjs(0))
-  const [activityValue, setActivityValue] = React.useState<string>('')
-  const [targetValue, setTargetValue] = React.useState<any>(0)
-  const [progressValue, setProgressValue] = React.useState<Number>(0)
-  const [idValue, setIdValue] = React.useState<string>('')
+  const [startValue, setStartValue] = React.useState<Dayjs | null>(dayjs(0));
+  const [endValue, setEndValue] = React.useState<Dayjs | null>(dayjs(0));
+  const [activityValue, setActivityValue] = React.useState<string>("");
+  const [targetValue, setTargetValue] = React.useState<any>(0);
+  const [progressValue, setProgressValue] = React.useState<Number>(0);
+  const [idValue, setIdValue] = React.useState<string>("");
 
-  let localGoal: Goal[]
-  let localGoalString: string | null
+  let localGoal: Goal[];
+  let localGoalString: string | null;
   useEffect(() => {
-    localGoalString = localStorage.getItem('goal')
+    localGoalString = localStorage.getItem("goal");
     if (goals && goals.length == 1 && !localGoalString) {
-      localStorage.setItem('goal', JSON.stringify(goals))
-      localGoalString = localStorage.getItem('goal')
+      localStorage.setItem("goal", JSON.stringify(goals));
+      localGoalString = localStorage.getItem("goal");
       if (localGoalString) {
-        localGoal = JSON.parse(localGoalString)
+        localGoal = JSON.parse(localGoalString);
       }
     } else if (goals && localGoalString)
       if (localGoalString) {
-        localGoal = JSON.parse(localGoalString)
+        localGoal = JSON.parse(localGoalString);
       }
 
     if (localGoal) {
-      setIdValue(localGoal[0]._id)
-      setStartValue(dayjs(localGoal[0].start))
-      setEndValue(dayjs(localGoal[0].end))
-      setActivityValue(localGoal[0].activity)
-      setTargetValue(localGoal[0].target)
-      setProgressValue(localGoal[0].progress)
+      setIdValue(localGoal[0]._id);
+      setStartValue(dayjs(localGoal[0].start));
+      setEndValue(dayjs(localGoal[0].end));
+      setActivityValue(localGoal[0].activity);
+      setTargetValue(localGoal[0].target);
+      setProgressValue(localGoal[0].progress);
     }
-  }, [goals])
+  }, [goals]);
 
   const schema = z.object({
     start: z.any(),
@@ -86,21 +134,21 @@ export default function ActivityInputForm() {
     // refactor to use only portion of date needed, so this can be more specific on the type
     activity: z
       .string()
-      .min(3, { message: 'Please update, minimum length is 3 characters' }),
+      .min(3, { message: "Please update, minimum length is 3 characters" }),
     target: z.number().nonnegative(),
     progress: z
       .number()
       .nonnegative()
-      .min(0, { message: 'Value can not be less than 0' })
+      .min(0, { message: "Value can not be less than 0" })
       .max(targetValue, {
-        message: 'Value can not be larger than your target',
+        message: "Value can not be larger than your target",
       }),
-  })
+  });
 
-  type Inputs = z.infer<typeof schema>
+  type Inputs = z.infer<typeof schema>;
 
-  const navigate = useNavigate()
-  const dispatch = useAppDispatch()
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
 
   const {
     register,
@@ -108,7 +156,7 @@ export default function ActivityInputForm() {
     formState: { errors },
   } = useForm<Inputs>({
     resolver: zodResolver(schema),
-  })
+  });
 
   const onSubmit: SubmitHandler<Inputs> = (data) => {
     if (goals) {
@@ -119,129 +167,179 @@ export default function ActivityInputForm() {
         activity: data.activity,
         target: data.target,
         progress: data.progress,
-      }
-      dispatch(changeGoal(goalData))
-      dispatch(getGoals(user))
-      setChangeBtn(false)
-      localStorage.removeItem('goal')
-      navigate(`/goals/user/${user.name}`)
-      window.location.reload()
+      };
+      dispatch(changeGoal(goalData));
+      dispatch(getGoals(user));
+      setChangeBtn(false);
+      localStorage.removeItem("goal");
+      navigate(`/goals/user/${user.name}`);
+      window.location.reload();
     }
-  }
+  };
 
   return activityValue ? (
-    <Paper elevation={14} className="form-container">
-      <Typography variant="h5" component="h5" align="center">
-        Update Goal
-      </Typography>
-      {changeBtn ? (
-        <Box
-          component="form"
-          className="form"
-          noValidate
-          onSubmit={handleSubmit(onSubmit)}
-        >
-          <Grid container spacing={0}>
-            <Grid item xs={12} className="dateLabel">
-              <DemoItem label="Start Date">
-                <DatePicker
-                  value={dayjs(startValue)}
+    <ThemeProvider theme={theme}>
+      <Paper elevation={14} className="form-container">
+        <Typography variant="h5" component="h5" align="center">
+          Update Goal
+        </Typography>
+        {changeBtn ? (
+          <Box
+            component="form"
+            className="form"
+            noValidate
+            onSubmit={handleSubmit(onSubmit)}
+          >
+            <Grid container spacing={0}>
+              <Grid item xs={12} sx={{ paddingTop: "1rem" }}>
+                <div className="goalText">Beginning </div>
+                <MobileDatePicker
+                  format="dddd MMMM DD, YYYY"
+                  value={startValue}
                   onChange={(newValue) => setStartValue(newValue)}
-                />
-              </DemoItem>
-            </Grid>
-            <Grid item xs={12} className="dateLabel">
-              <DemoItem label="Completion Date">
-                <DatePicker
-                  value={dayjs(endValue)}
-                  onChange={(newValue) => setEndValue(newValue)}
-                />
-              </DemoItem>
-            </Grid>
-            <Grid item xs={12} className="dateLabel">
-              <DemoItem label="Activity Name">
-                <TextField
-                  required
-                  fullWidth
-                  variant="filled"
-                  margin="normal"
-                  id="activity"
-                  defaultValue={activityValue}
-                  {...register('activity', {
-                    onChange: (e) => {
-                      setActivityValue(e.target.value)
+                  sx={{
+                    "& fieldset": { border: "none" },
+                    "& .MuiInputBase-input": {
+                      padding: "0",
+                      paddingLeft: "15px",
+                      width: "250px",
+                      fontSize: "1em",
+                      cursor: "pointer",
                     },
-                  })}
+                    borderBottom: "2px #1e5b5e solid",
+                    borderRadius: "0",
+                    "&:hover": {
+                      borderBottom: "2px #61dafb solid",
+                      borderRadius: "0",
+                    },
+                  }}
                 />
-                <div style={{ color: 'red' }}>{errors.activity?.message}</div>
-              </DemoItem>
-            </Grid>
-            <Grid item xs={12} className="dateLabel">
-              <DemoItem label="Target">
+                {/* {errors.start?.message ? (
+                <div style={{ color: "red" }}>{errors.start?.message}</div>
+              ) : (
+                ""
+              )} */}
+
+                <div className="goalText"> through </div>
+                <MobileDatePicker
+                  format="dddd MMMM DD, YYYY"
+                  value={endValue}
+                  onChange={(newValue) => setEndValue(newValue)}
+                  sx={{
+                    "& fieldset": { border: "none" },
+                    "& .MuiInputBase-input": {
+                      padding: "0",
+                      paddingLeft: "15px",
+                      width: "250px",
+                      fontSize: "1em",
+                      cursor: "pointer",
+                    },
+                    borderBottom: "2px #1e5b5e solid",
+                    borderRadius: "0",
+                    "&:hover": {
+                      borderBottom: "2px #61dafb solid",
+                      borderRadius: "0",
+                    },
+                  }}
+                />
+                {/* {errors.end?.message ? (
+                <div style={{ color: "red" }}>{errors.end?.message}</div>
+              ) : (
+                ""
+              )} */}
+              </Grid>
+
+              <Grid item xs={12} sx={{ paddingTop: "1rem" }}>
+                <div className="goalText">I plan to commit </div>
                 <TextField
                   required
-                  fullWidth
-                  variant="filled"
-                  margin="normal"
+                  variant="standard"
                   type="number"
                   id="Number of hours to complete goal"
                   defaultValue={targetValue}
-                  {...register('target', {
+                  {...register("target", {
                     valueAsNumber: true,
                     onChange: (e) => {
-                      setTargetValue(e.target.value)
+                      setTargetValue(e.target.value);
                     },
                   })}
+                  sx={{
+                    width: "50px",
+                  }}
                 />
-                <div style={{ color: 'red' }}>{errors.target?.message}</div>
-              </DemoItem>
-            </Grid>
-            <Grid item xs={12} className="dateLabel">
-              <DemoItem label="Progress">
+                <div className="goalText">hours achieving my goal of:</div>
+                <div style={{ color: "red" }}>{errors.target?.message}</div>
+              </Grid>
+              <Grid item xs={12} sx={{ paddingTop: "1rem" }}>
+                <div className="goalText">
+                  <TextField
+                    required
+                    fullWidth
+                    variant="filled"
+                    id="activity"
+                    defaultValue={activityValue}
+                    {...register("activity", {
+                      onChange: (e) => {
+                        setActivityValue(e.target.value);
+                      },
+                    })}
+                    sx={{
+                      maxWidth: "500px",
+                    }}
+                  />
+
+                  <div style={{ color: "red" }}>{errors.activity?.message}</div>
+                </div>
+              </Grid>
+              <Grid item xs={12} sx={{ padding: "1rem" }}>
+                <div className="goalText">So far I've completed </div>
                 <TextField
                   required
                   fullWidth
-                  variant="filled"
-                  margin="normal"
+                  variant="standard"
                   type="number"
                   id="progress"
                   defaultValue={progressValue}
-                  {...register('progress', {
+                  {...register("progress", {
                     valueAsNumber: true,
                     onChange: (e) => {
-                      setProgressValue(e.target.value)
+                      setProgressValue(e.target.value);
                     },
                   })}
+                  sx={{
+                    width: "100px",
+                  }}
                 />
-                <div style={{ color: 'red' }}>{errors.progress?.message}</div>
-              </DemoItem>
+                <div className="goalText"> hours </div>
+                <div style={{ color: "red" }}>{errors.progress?.message}</div>
+              </Grid>
+              <Button
+                type="submit"
+                variant="contained"
+                color="primary"
+                className="btn"
+              >
+                Save Changes
+              </Button>
             </Grid>
-            <Button
-              type="submit"
-              variant="contained"
-              color="primary"
-              className="btn"
-            >
-              Save Changes
-            </Button>
-          </Grid>
-        </Box>
-      ) : (
-        <Box className="form">
-          <Link to={`/goals/user/${user._id}`}>
-            <Button
-              onClick={() => {
-                setChangeBtn(true)
-              }}
-              variant="contained"
-            >
-              Changes saved - Return to Dashboard
-            </Button>
-          </Link>
-        </Box>
-      )}
-    </Paper>
+          </Box>
+        ) : (
+          <Box className="form">
+            <Link to={`/goals/user/${user._id}`}>
+              <Button
+                onClick={() => {
+                  setChangeBtn(true);
+                }}
+                variant="contained"
+              >
+                Changes saved - Return to Dashboard
+              </Button>
+            </Link>
+          </Box>
+        )}
+      </Paper>
+    </ThemeProvider>
   ) : (
     <></>
-  )
+  );
 }
